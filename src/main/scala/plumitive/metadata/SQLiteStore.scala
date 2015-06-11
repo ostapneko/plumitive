3f239d5c8dae54b81cb76f6b333f2ca47768a3d3
Thomas Franquelin <ostapneko@gmail.com>
2015-06-11 21:30:26 +0100

    Configure database path

--- src/main/scala/plumitive/metadata/SQLiteStore.scala
+++ src/main/scala/plumitive/metadata/SQLiteStore.scala
@@ -8,7 +8,7 @@ import scala.concurrent.Future
 
 object SQLiteStore extends Store {
   implicit val ec = Settings.executionContext
-  val db = Database.forConfig("sqlitePlumitive")
+  val db = Database.forConfig("plumitive.sqliteDB")
 
   def query(searchQuery: SearchQuery): Future[Seq[Document]] = {
     val q = for ( d <- Schema.documents ) yield d
--- src/main/scripts/migrate.py
+++ src/main/scripts/migrate.py
@@ -1,12 +1,21 @@
+#!/usr/bin/env python
+
 import sqlite3
 import os
+import re
 
 migration_dir = os.path.dirname(os.path.realpath(__file__))
+resource_dir = os.path.join(migration_dir, '..', 'resources')
+sqlite_db_line = open(resource_dir + '/sqlite_db_path.conf', 'r').read()
+
+db_path = re.findall("plumitive.sqliteDB.dbPath\s*=\s*\"(.*)\"", sqlite_db_line)[0]
+
 qry = open(migration_dir + '/migration.sql', 'r').read()
 
 # Create and connect to the database
-conn = sqlite3.connect('plumitive.db')
-print("Created local plumitive database")
+print("Attempt to create the database at " + db_path)
+conn = sqlite3.connect(db_path)
+print("Created plumitive database")
 
 #Create cursor
 c = conn.cursor()
